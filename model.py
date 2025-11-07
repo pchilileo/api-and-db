0fb276ab75c8f4d0f45064f8a66e002924a806bb
pchil <olegcigrin83@gmail.com>
2025-11-07 18:38:01 +0500

    alpha v0.1
    created that api + db for testing and learning

---
+++ model.py
@@ -0,0 +1,14 @@
+from sqlmodel import Field, SQLModel
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    login: str
+    password: str
+
+class Task(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="user.id")
+    name: str
+    description: str | None = None
+    deadline: str | None = None
+    status: bool = Field(default=False) 
