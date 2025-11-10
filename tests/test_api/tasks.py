1947f690bc292d513eca818a275e046cd4305c05
pchil <olegcigrin83@gmail.com>
2025-11-10 23:08:15 +0500

    1
    1

--- tests/test_api/tasks.py
+++ tests/test_api/tasks.py
@@ -1,10 +1,11 @@
 from fastapi import Depends, APIRouter
 from app.models.task import Task
 from app.database import get_session
+from sqlmodel import select
 
 router = APIRouter()
 
 @router.get("/tasks/")
 def get_tasks(Session = Depends(get_session)):
-    tasks = Session.query(Task).all()
+    tasks = select(Task).all()
     return tasks
